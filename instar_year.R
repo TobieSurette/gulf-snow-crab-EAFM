989ad98a5afba8476e70512999f146e1c630d0c2
TobieSurette <tobie.surette@gmail.com>
2020-11-16 12:23:13 -0400

    Stepwise female instar fitting

--- instar_year.R
+++ instar_year.R
@@ -2,12 +2,12 @@ library(TMB)
 library(gulf.data)
 library(gulf.graphics)
 
+category <- "FI"
 years  <- 1998:2020
-instars <- 4:11
-mu_instars <- c(10.0, 13.9, 19.4, 27.1, 37.8, 48.8, 66.8, 87.0)
+mu_instars <- c(10.5, 14.5, 20.5, 28.1, 37.8, 51.1, 68.0, 88.0)
 names(mu_instars) <- 4:11
-mu_instars <- mu_instars[as.character(instars)]
-
+roman <- c("I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X", "XI", "XII", "XIII", "XIV", "XV")
+   
 # Work computer fix:
 if (Sys.getenv("RSTUDIO_USER_IDENTITY") == "SuretteTJ") Sys.setenv(BINPREF = "C:/Rtools/mingw_64/bin/")
 
@@ -16,83 +16,149 @@ dyn.load(dynlib("instar_year"))
 
 # Define data:
 b <- read.scsbio(years, survey = "regular")
-b <- b[which(is.category(b, "MI")), ]
-b <- b[which(b$carapace.width > 10), ]
+b <- b[which(is.category(b, category)), ]
+b <- b[which(b$carapace.width > 6), ]
+if (b$sex[1] == "2"){
+   b <- b[which(b$carapace.width <= 90), ] 
+   mu_instars <- mu_instars[as.character(4:9)]
+}else{
+   b <- b[which(b$carapace.width <= 140), ]
+} 
 b$year <- year(b)
 
+gbarplot(log(table(round(log(b$carapace.width),2))+1), width = 0.01, border = "grey50")
+vline(log(mu_instars))
+
+# Set up data:
 r <- aggregate(list(f = b$year), list(x = round(log(b$carapace.width), 2), year = b$year), length)
 data <- list(x = r$x, f = r$f, year = r$year - min(r$year))
    
 # Define initial parameters:
 parameters <- list(mu_instar_0 = as.numeric(log(mu_instars[1])),   # Mean size of the first instar.
-                   log_increment = as.numeric(log(diff(log(mu_instars[1:length(instars)])))), # Vector of log-scale growth increments.
+                   log_increment = as.numeric(log(diff(log(mu_instars)))), # Vector of log-scale growth increments.
                    log_mu_increment = -1.165,          # Log-scale mean parameter associated with instar growth increments.
                    log_sigma_increment = -3.42,        # Log-scale error parameter associated with instar growth increments.
                    mu_year = rep(0, length(years)),    # Annual instar mean deviations.
                    log_sigma_mu_year = -4,             # Log-scale error for annual instar mean deviations.
                    log_sigma_mu_instar_year = -4, 
-                   mu_instar_year = rep(0, length(instars) * length(years)), 
+                   mu_instar_year = rep(0, length(mu_instars) * length(years)), 
                    mu_log_sigma_instar = -2.24,        # Instar errors log-scale mean. 
                    log_sigma_log_sigma_instar = -2.44, # Instar errors log-scale error.
-                   log_sigma_instar = rep(-2.41, length(instars)),   # Log-scale instar error parameters.
+                   log_sigma_instar = rep(-2.41, length(mu_instars)),   # Log-scale instar error parameters.
                    mu_logit_p = 4,                     # Instar proportions log-scale mean parameter.
                    log_sigma_logit_p_instar_year = -1, # Instar proportions log-scale error parameter.
-                   logit_p_instar_year = rep(0, (length(instars)-1) * length(years))) # Multi-logit-scale parameters for instar proportions by year.
+                   logit_p_instar_year = rep(0, (length(mu_instars)-1) * length(years))) # Multi-logit-scale parameters for instar proportions by year.
    
 obj <- MakeADFun(data, parameters, DLL = "instar_year", 
-                 random = c("mu_year", "mu_instar_year", "log_sigma_instar", "log_increment", "logit_p_instar_year")) 
+                 random = c("mu_year", "mu_instar_year", "log_sigma_instar", "log_increment", "logit_p_instar_year"),
+                 map = list(mu_instar_0 = factor(NA), 
+                            log_increment = factor(rep(NA, length(parameters$log_increment))),
+                            log_sigma_mu_year = factor(NA),
+                            log_sigma_mu_instar_year = factor(NA),
+                            mu_year = factor(rep(NA, length(parameters$mu_year))))) 
    
 # Estimate parameters:
 theta <- optim(obj$par, obj$fn, control = list(trace = 3, maxit = 1000))$par
-   
-# Parse output:
 obj$par <- theta
 rep <- sdreport(obj)
-phi <- summary(rep, "random")
-theta <- summary(rep, "fixed")
+parameters <- update.parameters(parameters, summary(rep, "fixed"), summary(rep, "random"))
    
+obj <- MakeADFun(data, parameters, DLL = "instar_year", 
+                 random = c("mu_year", "mu_instar_year", "log_sigma_instar", "log_increment", "logit_p_instar_year"),
+                 map = list(mu_instar_0 = factor(NA), 
+                            log_sigma_mu_year = factor(NA),
+                            log_sigma_mu_instar_year = factor(NA),
+                            mu_year = factor(rep(NA, length(parameters$mu_year))))) 
+
+theta <- optim(obj$par, obj$fn, control = list(trace = 3, maxit = 1000))$par
+obj$par <- theta
+rep <- sdreport(obj)
+parameters <- update.parameters(parameters, summary(rep, "fixed"), summary(rep, "random"))
+
+obj <- MakeADFun(data, parameters, DLL = "instar_year", 
+                 random = c("mu_year", "mu_instar_year", "log_sigma_instar", "log_increment", "logit_p_instar_year"),
+                 map = list(log_sigma_mu_year = factor(NA),
+                            log_sigma_mu_instar_year = factor(NA),
+                            mu_year = factor(rep(NA, length(parameters$mu_year))))) 
+
+theta <- optim(obj$par, obj$fn, control = list(trace = 3, maxit = 1000))$par
+obj$par <- theta
+rep <- sdreport(obj)
+parameters <- update.parameters(parameters, summary(rep, "fixed"), summary(rep, "random"))
+
+
+# 
+obj <- MakeADFun(data, parameters, DLL = "instar_year", 
+                 random = c("mu_year", "mu_instar_year", "log_sigma_instar", "log_increment", "logit_p_instar_year"))
+
+
+theta <- optim(obj$par, obj$fn, control = list(trace = 3, maxit = 1000))$par
+obj$par <- theta
+rep <- sdreport(obj)
+parameters <- update.parameters(parameters, summary(rep, "fixed"), summary(rep, "random"))
 
 clg()
-m <- kronecker(matrix(1:24, ncol = 3), matrix(1, ncol = 4, nrow = 4))
+windows(width = 8.5, height = 11)
+m <- kronecker(matrix(1:24, ncol = 3), matrix(1, ncol = 3, nrow = 3))
 m <- rbind(0, cbind(0, 0, m, 0), 0, 0)
 layout(m)
 par(mar = c(0,0,0,0))
 m <- s <- p <- NULL
 for (i in 1:length(years)){   
    # Plot output:
-   gbarplot(data$f[data$year == i-1], data$x[data$year == i-1], border = "grey50", width = 0.01, xlim = c(2, 5), xaxs = "i", xaxt = "n", yaxt = "n")
+   gbarplot(data$f[data$year == i-1], data$x[data$year == i-1], 
+            border = "grey50", width = 0.01, xlim = c(2.0, 4.5), 
+            xaxs = "i", xaxt = "n", yaxt = "n", ylim = c(0, 150), lwd = 0.5)
    grid()
-   vline(obj$report()$mu[,i], col = "red", lwd = 2)
+   vline(obj$report()$mu[,i], col = "blue", lwd = 0.5)
    x0 <- seq(0, 5, len = 1000)
    d <- rep(0, length(x0))
    for (j in 1:length(obj$report()$mu_instar)){
-      lines(x0, 0.01 * obj$report()$p[j,i] * sum(data$f[data$year == i-1]) * dnorm(x0, obj$report()$mu[j,i], obj$report()$sigma_instar[j]), lwd = 2, col = "blue")
+      lines(x0, 0.01 * obj$report()$p[j,i] * sum(data$f[data$year == i-1]) * dnorm(x0, obj$report()$mu[j,i], obj$report()$sigma_instar[j]), lwd = 0.5, lty = "dashed", col = "blue")
       d <- d + .01 * obj$report()$p[j,i] * sum(data$f[data$year == i-1]) * dnorm(x0, obj$report()$mu[j,i], obj$report()$sigma_instar[j]) 
    }
-   lines(x0, d, col = "darkgreen", lwd = 2)
+   lines(x0, d, col = "blue", lwd = 0.5)
    
-   vline(obj$report()$mu_instar, col = "green", lwd = 2)
-   
-   if ((i / length(years)) <= 0.5) axis(2)
-   if (i %in% c(length(years)/2, length(years))) axis(1)
+
+   if (i <= 8) axis(2)
+   if (i == 8) axis(1, at = seq(2, 4.0, by = 0.5))
+   if (i == 16) axis(1, at = seq(2, 4.5, by = 0.5))
+   if (i == length(years)) axis(1, at = seq(2.5, 4.5, by = 0.5))
    if (i == length(years)/2) mtext("log(cw)", 1, 2.5, at = par("usr")[2])
-   if (i == 3) mtext("Frequency", 2, 2, cex = 1.25, at = 0)
+   if (i == 4) mtext("Frequency", 2, 2.5, cex = 1.25, at = 0)
+   if (i == 16) mtext("ln(cw)", 1, 2.5, cex = 1.25)
    text(par("usr")[1] + 0.1 * diff(par("usr")[1:2]),
         par("usr")[3] + 0.85 * diff(par("usr")[3:4]), years[i], cex = 1.2)
+   box()
 }
 
 
 mu_instar_year <- phi[grep("mu_instar_year", rownames(phi)), 1]
-dim(mu_instar_year) <- c(length(years), length(instars))
-dimnames(mu_instar_year) <- list(year = years, instar = instars)
+dim(mu_instar_year) <- c(length(years), length(mu_instars))
+dimnames(mu_instar_year) <- list(year = years, instar = names(mu_instars))
 
-image(years, instars, mu_instar_year, breaks = seq(-0.25, 0.25, len = 101), col = colorRampPalette(c("red", "white", "blue"))(100))
+image(years, as.numeric(names(mu_instars)), mu_instar_year, breaks = seq(-0.25, 0.25, len = 101), col = colorRampPalette(c("red", "white", "blue"))(100))
 
 clg()
-v <- obj$report()$mu
-plot(range(years), c(10, 100), type = "n")
+mu <- obj$report()$mu
+dimnames(mu) <- list(names(mu_instars), years)
+
+plot(range(years), c(10, 55), type = "n", xlab = "Years", ylab = "Carapace width (mm)")
 for (i in 1:length(years)){
-   points(rep(years[i], nrow(v)), exp(v[, i]))
+   points(rep(years[i], nrow(mu)), exp(mu[, i]))
 }
+
+
+dmu <- exp(mu) - repvec(apply(exp(mu), 1, mean), ncol = ncol(mu))
+clg()
+colorbar(levels = seq(-3, 3, by = 1), color = colorRampPalette(c("red", "white", "blue"))(6),
+         caption = c("difference(mm)"), smooth = TRUE)
+
+image(years, as.numeric(names(mu_instars)), t(dmu), 
+      breaks = seq(-3, 3, len = 101), 
+      col = colorRampPalette(c("red", "white", "blue"))(100),
+      xlab = "Years", ylab = "Instar")
+box()
+
 
 
