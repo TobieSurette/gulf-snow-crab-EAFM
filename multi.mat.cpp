2b053f33e464e7f604f984057a131b811dc0579e
Tobie Surette <tobie.surette@gmail.com>
2020-12-16 07:37:53 -0400

    Update multi.mat.cpp

--- multi.mat.cpp
+++ multi.mat.cpp
@@ -3,33 +3,23 @@ matrix<Type> n_imm(n_instar,n_year);   // Immatures.
 matrix<Type> n_skp(n_instar,n_year);   // Immature skip-moulters.
 array<Type>  n_mat(n_instar,n_year,6); // Matures.
 
+n_ma
 // Mature recruits for first year and first instar: 
 for (int k = 0; k < 5; k++){   
-   n_mat(k,0,0) = 0; 
+   for (int a = 0; a < 6; a++){ 
+      n_mat(k,0,a) = 0; // First year small mature recruits set to zero.
+   }
 } 
-for (int k = 5; k < n_instar; k++){
-   n_mat(k,0,0) = exp(log_n_rec_instar_0[k-5]);  
-}
 for (int y = 1; y < n_year; y++){ 
-   n_mat(0,y,0) = 0;  
+   n_mat(0,y,0) = 0; // First instar mature recruits set to zero.
+}
+for (int k = 5; k < n_instar; k++){
+   for (int a = 0; a < 6; a++){ 
+      
+      n_mat(k,0,a) = exp(log_n_rec_instar_0();  // Mature abundances for first year. 
+   }
 }
 
-// First year:
-for (int k = 0; k < 5; k++){   
-   n_mat(k,0,1) = 0; 
-   n_mat(k,0,2) = 0;
-   n_mat(k,0,3) = 0;
-   n_mat(k,0,4) = 0;
-   n_mat(k,0,5) = 0;
-} 
-// Matures first year:
-for (int k = 5; k < n_instar; k++){
-   n_mat(k,0,1) = exp(log_n_res_instar_0[k-5]);  
-   n_mat(k,0,2) = exp(log_n_res_instar_0[k-5]);
-   n_mat(k,0,3) = exp(log_n_res_instar_0[k-5]);
-   n_mat(k,0,4) = exp(log_n_res_instar_0[k-5]);
-   n_mat(k,0,5) = exp(log_n_res_instar_0[k-5]);
-}
 // First instar:
 for (int y = 1; y < n_year; y++){ 
    n_mat(0,y,1) = 0;
