be0299586ef70450a5b3769c0f053e8a7561f336
Tobie Surette <tobie.surette@gmail.com>
2021-05-11 12:20:36 -0300

    Figure updates

---
+++ model/mixture by group/histogram figures.R
@@ -0,0 +1,67 @@
+ylim = c(0, 30)
+
+groups <- tows$tow[rev(order(tows$n))][1:1000]
+
+r <- obj$report()
+mu <- r$mu
+dimnames(mu) <- list(instar = names(mu_instars), tow = tows$tow)
+p <- r$p
+dimnames(p) <- list(instar = names(mu_instars), tow = tows$tow)
+sigma <- r$sigma
+dimnames(sigma) <- list(instar = names(mu_instars), tow = tows$tow)
+
+clg()
+file <- paste0("figures/Female immature histograms.pdf")
+pdf(file = file, width = 8.5, height = 8.5)
+m <- kronecker(matrix(1:10, ncol = 2), matrix(1, ncol = 5, nrow = 5))
+m <- rbind(0, cbind(0, 0, m, 0), 0, 0)
+layout(m)
+par(mar = c(0,0,0,0))
+ix <- data$group %in% groups
+tmp <- lapply(data, function(x) if (length(x) == length(ix)) return(x[ix]) else return(x))
+
+for (i in 1:length(groups)){   
+   ix <- which(tmp$group == groups[i])
+   if (i %% 10 == 0){
+      ylim <- c(0, 10*(floor(sum(tmp$f[ix]) / 300) + 1))
+   }
+   if (length(ix) > 1){
+      # Plot output:
+      z <- rep(exp(tmp$x[ix]), tmp$f[ix])
+      z <- z + rep(tmp$precision[ix], tmp$f[ix]) * (runif(length(z))-0.5)
+      tab <- table(round(log(z), 2))
+      gbarplot(tab, 
+               border = "grey50", width = 0.1, xlim = xlim, 
+               xaxs = "i", xaxt = "n", yaxt = "n", ylim = ylim, lwd = 0.5)
+      grid()
+      x0 <- seq(0, 5, by = 0.01)
+      d <- rep(0, length(x0))
+      for (j in 1:nrow(mu)){
+         a <- 0.01 * sum(tmp$f[ix]) * p[j,as.character(groups[i])] * dnorm(x0, mu[j,as.character(groups[i])], sigma[j,as.character(groups[i])])
+         lines(x0, a, lwd = 0.5, lty = "dashed", col = "blue")
+         d <- d + a
+      }
+      lines(x0, d, col = "blue", lwd = 0.5)
+   
+      v <- mu[,as.character(groups[i])]
+      ix <- match(round(v, 2), round(x0,2))
+      vline(v, col = "blue", label = paste0(round(exp(v),1), "mm"), at = d[ix] + 0.05 * diff(par("usr")[3:4]), cex = 0.8,
+            lwd = 0.5, lty = "dashed")
+         
+      if ((i %% 10) <= 5 & (i != 10)) axis(2)
+      if ((i %% 10) ==  5) axis(1, at = seq(2, xlim[2]-0.5, by = 0.5))
+      if ((i %% 10) ==  0) axis(1, at = seq(2.5, xlim[2], by = 0.5))
+      if ((i %% 10) ==  0) mtext("log(cw)", 1, 2.5, at = 2.5)
+      if ((i %% 10) == 3)  mtext("Frequency", 2, 2.5, cex = 1.25, at = mean(par("usr")[3:4]))
+
+      # Group label:
+      ix <- which(tows$tow == groups[i])
+      label <- paste0(tows$date[ix], ", tow #", tows$tow.number[ix], " (n = ",  tows$n[ix], ")")
+      text(par("usr")[1] + 0.5 * diff(par("usr")[1:2]), 
+           par("usr")[3] + 0.9 * diff(par("usr")[3:4]), label, pos = 1, cex = 0.80)
+      
+      box()
+   }
+}
+dev.off()   
+   
--- model/mixture by group/map.group.R
+++ model/mixture by group/map.group.R
@@ -12,15 +12,19 @@ dimnames(p) <- list(instar = names(mu_instars), tow = 0:(ncol(mu)-1))
 sigma <- r$sigma
 dimnames(sigma) <- list(instar = names(mu_instars), tow = 0:(ncol(mu)-1))
 
-c(10.5, 14.5, 20.5, 28.1, 37.8, 51.1, 68.0, 88.0)
+# Calculate instar counts:
+n <- p * repvec(tows$n, nrow = data$n_instar)
+d <- 1000000 * n / repvec(tows$swept.area, nrow = data$n_instar)
+#c(10.5, 14.5, 20.5, 28.1, 37.8, 51.1, 68.0, 88.0)
 
+# Plot instar size anomalies:
 years <- sort(unique(data$year))
 instars <- 4:9
 for (j in 1:length(years)){
    for (i in 1:length(instars)){
       scale <- 1.5 * sqrt((50 / mu_instars[as.character(instars[i])]))
       limit <- 2.5 #round(3 * (50 / mu_instars[as.character(instars[i])]), 1)
-      s <- read.scsset(years[j], valid = 1, valid = 1)   
+      s <- read.scsset(years[j], valid = 1, survey = "regular")   
       
       # Instars:
       s$tow <- tows$tow[match(s[vars], tows[vars])] 
@@ -28,7 +32,7 @@ for (j in 1:length(years)){
       s$value <- NA
       s$value[ix] <- exp(mu[as.character(instars[i]), as.character(s$tow[ix])]) - exp(r$mu_instar)[instars[i]-3]
 
-      file <- paste0("maps/instar size/Female immature ", years[j], " - instar ", as.roman(instars[i]), ".pdf")
+      file <- paste0("maps/instar size/Female immature ", years[j], " - instar ", as.roman(instars[i]), " size anomaly.pdf")
       pdf(file = file, width = 8.5, height = 8.5)
       map.new()
       #map("bathymetry")
@@ -54,5 +58,79 @@ for (j in 1:length(years)){
    }
 }
    
+# Plot instar densitiess:
+years <- sort(unique(data$year))
+instars <- 4:9
+for (j in 1:length(years)){
+   for (i in 1:length(instars)){
+      scale <- 0.02
+      limit <- 20000
+      s <- read.scsset(years[j], valid = 1, survey = "regular")   
+      
+      # Instars:
+      s$tow <- tows$tow[match(s[vars], tows[vars])] 
+      ix <- !is.na(as.character(s$tow))
+      s$value <- NA
+      s$value[ix] <- d[as.character(instars[i]), as.character(s$tow[ix])]
+
+      file <- paste0("maps/instar density/Female immature ", years[j], " - instar ", as.roman(instars[i]), " density.pdf")
+      pdf(file = file, width = 8.5, height = 8.5)
+      map.new()
+      #map("bathymetry")
+      map("coast")
+      ix <- s$value > 0
+      points(lon(s)[ix], lat(s)[ix], pch = 21, cex = scale * sqrt(s$value[ix]), bg = "red3")
+      ix <- which(is.na(s$value)) 
+      points(lon(s)[ix], lat(s)[ix], pch = "x", lwd = 2)
+      box()
+      map.axis(1:2)
+      v <- round(seq(0, limit, len = 4),1)
+      cex = scale * sqrt(abs(v))
+      cex[cex == 0] <- 1
+      legend("bottomleft", 
+             legend = paste0(round(rev(v)), " #/km2"),
+             pch = c(4, 21, 21, 21),
+             pt.bg = c("black", "red3", "red3", "red3"),
+             pt.cex = cex, bg = "white")
+     # mtext(paste0(unique(data$year), " survey, instar ", as.roman(i), ", mean size = ", round(exp(r$mu_instar)[i-3], 1), " mm"),
+   #         3, 0.5, cex = 1.5)
+      dev.off()
+   }
+}
+
+# Annual log-scale size differencesL
+clg()
+dev.new(width = 8.5, height = 11)
+m <- kronecker(matrix(1:6, ncol = 1), matrix(1, ncol = 5, nrow = 5))
+m <- rbind(0, 0, cbind( 0, m, 0), 0, 0)
+layout(m)
+par(mar = c(0,0,0,0))
+for (i in 1:nrow(mu)){
+   if (i == 4) mtext("Log-scale size anomaly", 2, 2.5, cex = 1.25, at = par("usr")[3])
+   z <- mu[nrow(mu)-i+1, ] - r$mu_instar[nrow(mu)-i+1]
+   boxplot(z ~ year(tows$date), cex = 0.2, xlab = "", ylab = "", xaxt = "n", ylim = c(-.08, 0.08))
+   mtext(as.roman(nrow(mu)-i+1+3), 4, 1, cex = 1.25)
+}
+axis(1, at = seq(3, 33, by = 5), labels = years[seq(3, 33, by = 5)])
+mtext("Year", 1, 2.75, cex = 1.5)
+
+
+
+clg()
+dev.new(width = 8.5, height = 11)
+m <- kronecker(matrix(1:36, ncol = 6), matrix(1, ncol = 5, nrow = 5))
+m <- rbind(0, cbind(0, 0, m, 0), 0, 0)
+layout(m)
+par(mar = c(0,0,0,0))
+for (i in 1:6){
+   for (j in 1:6){
+      plot(mu[i, ], mu[j, ], cex = 0.1)
+   }
+}
+
+
+
+
+
 
 
